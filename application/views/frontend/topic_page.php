d9b19d99d06ac3c091d34bc29aa27c2ee02157af
Rana <ranacoregen@gmail.com>
2018-08-09 20:03:09 +0530

    #15-Clickable Images

--- application/views/frontend/topic_page.php
+++ application/views/frontend/topic_page.php
@@ -135,6 +135,25 @@ $this->load->view('templates/header');
 				                                <?php $option_counter++; ?>
 			                                <?php } ?>
                                         </div>
+	                                <?php } elseif ($form_data['question_option']=='5') { ?>
+		                                <?php //print_r($form_data); ?>
+                                        <div class="option_list_d">
+			                                <?php //print_r($form_data['option_arrange']); ?>
+                                            <div class="option_list_d onlyclickable" id="sortable">
+				                                <?php $option_counter=1; ?>
+				                                <?php foreach ($form_data['option_dearrange'] as $option){ ?>
+                                                    <div class="inputGroup">
+                                                        <label><?php echo $option; ?></label>
+                                                        <input type="hidden" name="option_arrange[]" value="<?php echo $option; ?>"/>
+                                                    </div>
+					                                <?php $option_counter++; ?>
+				                                <?php } ?>
+                                                <script>
+                                                    jQuery( "#sortable" ).sortable();
+                                                    jQuery( "#sortable" ).disableSelection();
+                                                </script>
+                                            </div>
+                                        </div>
 	                                <?php } elseif ($form_data['question_option']=='6') { ?>
                                         <div class="option_list_d onlyclickable">
 			                                <?php $option_counter=1; ?>
@@ -170,40 +189,7 @@ $this->load->view('templates/header');
                                             <p>Answer 1: </p><span><input type="text" name="qAns_box" class="form-control"></span>
 
                                         </div>
-	                                <?php }  elseif ($form_data['question_option']=='14') { ?>
-		                                <?php //print_r($form_data); ?>
-                                        <div class="option_list_d">
-                                            <div class="option_list_d">
-		                                        <?php $option_counter=1; ?>
-		                                        <?php foreach ($form_data['option_1'] as $option){ ?>
-                                                    <div class="inputGroup">
-                                                        <input id="radio<?php echo $option_counter; ?>" name="option_1" value="<?php echo $option; ?>" type="radio"/>
-                                                        <label for="radio<?php echo $option_counter; ?>"><?php echo $option; ?></label>
-                                                    </div>
-			                                        <?php $option_counter++; ?>
-		                                        <?php } ?>
-                                            </div>
-                                        </div>
-	                                <?php }  elseif ($form_data['question_option']=='5') { ?>
-		                                <?php //print_r($form_data); ?>
-                                        <div class="option_list_d">
-                                            <?php //print_r($form_data['option_arrange']); ?>
-                                            <div class="option_list_d onlyclickable" id="sortable">
-				                                <?php $option_counter=1; ?>
-				                                <?php foreach ($form_data['option_dearrange'] as $option){ ?>
-                                                    <div class="inputGroup">
-                                                        <label><?php echo $option; ?></label>
-                                                        <input type="hidden" name="option_arrange[]" value="<?php echo $option; ?>"/>
-                                                    </div>
-					                                <?php $option_counter++; ?>
-				                                <?php } ?>
-                                                <script>
-                                                    jQuery( "#sortable" ).sortable();
-                                                    jQuery( "#sortable" ).disableSelection();
-                                                </script>
-                                            </div>
-                                        </div>
-	                                <?php } elseif ($form_data['question_option']=='13') { ?>
+	                                <?php }  elseif ($form_data['question_option']=='13') { ?>
 		                                <?php //print_r($form_data); ?>
                                         <div class="option_list_d">
 			                                <?php $option_counter=1; ?>
@@ -218,7 +204,45 @@ $this->load->view('templates/header');
                                                 <textarea class="form-control" style="width: 68%; height: 150px;" name="13_textbox"></textarea>
                                             </div>
                                         </div>
-	                                <?php } elseif ($form_data['question_option']=='20') { ?>
+	                                <?php }  elseif ($form_data['question_option']=='14') { ?>
+		                                <?php //print_r($form_data); ?>
+                                        <div class="option_list_d">
+                                            <div class="option_list_d">
+				                                <?php $option_counter=1; ?>
+				                                <?php foreach ($form_data['option_1'] as $option){ ?>
+                                                    <div class="inputGroup">
+                                                        <input id="radio<?php echo $option_counter; ?>" name="option_1" value="<?php echo $option; ?>" type="radio"/>
+                                                        <label for="radio<?php echo $option_counter; ?>"><?php echo $option; ?></label>
+                                                    </div>
+					                                <?php $option_counter++; ?>
+				                                <?php } ?>
+                                            </div>
+                                        </div>
+	                                <?php } elseif ($form_data['question_option']=='15') { ?>
+		                                <?php $img_array = explode('|',$form_data['img_array']); ?>
+		                                <?php if(!empty($img_array)){ ?>
+                                            <div class="row">
+                                                <input id="img_answer" type="hidden" name="img_answer" value="">
+				                                <?php $i = 1; ?>
+				                                <?php foreach($img_array as $img){ ?>
+					                                <?php
+					                                $reversedParts = explode('/', strrev($img), 2);
+					                                $img_name = strrev($reversedParts[0]);
+					                                ?>
+                                                    <div class="col-lg-6">
+                                                        <div class="form-group">
+                                                            <div class="imgselector">
+                                                                <label for="img_<?php echo $i; ?>">
+                                                                    <img data-img_name="<?php echo $img_name; ?>" src="<?php echo $img; ?>" class="img-thumbnail" style="max-width: 100%;width: auto;height: 150px;">
+                                                                </label>
+                                                                <!--<input id="img_<?php /*echo $i; */?>" type="radio" name="img_answer" value="<?php /*echo $img_name; */?>">-->
+                                                            </div>
+                                                        </div>
+                                                    </div>
+					                                <?php $i++;  } ?>
+                                            </div>
+		                                <?php } ?>
+	                                <?php }  elseif ($form_data['question_option']=='20') { ?>
                                         <div class="option_list_d onlyclickable">
                                             <div class="question_image">
                                                 <img src="<?php echo $form_data['img']; ?>" alt="">
