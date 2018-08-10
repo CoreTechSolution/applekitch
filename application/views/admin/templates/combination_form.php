9a7b2b10fb80b0ca169e2caff2e4de77f95fbaa9
CoreTechSolution <rktechnology2010@gmail.com>
2018-08-10 14:49:29 +0530

    #5 - Question with Arranging Option

--- application/views/admin/templates/combination_form.php
+++ application/views/admin/templates/combination_form.php
@@ -85,6 +85,19 @@
         </div>
     </div>
 </div>
+<!-- SelectMultibleImages -->
+<div id="qOption_8">
+    <div class="form-group">
+        <div class="row" id="upload_images_section">
+            <div class="col-lg-6">
+                <label for="upload_images">Upload Images</label>
+                <input type="file" multiple name="upload_images[]" class="form-control upload_select_multiple_images">
+            </div>
+            <div class="col-lg-6"></div>
+        </div>
+        <div id="uploaded_images" style="display: none;"></div>
+    </div>
+</div>
 <div id="qOption_9">
     <div class="form-group">
         <div class="row">
