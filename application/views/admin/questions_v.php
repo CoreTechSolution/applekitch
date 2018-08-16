2b9b3102326a6d37e460c559d48ca3326de267d9
Rana <ranacoregen@gmail.com>
2018-08-16 20:07:20 +0530

    Phase-4 Point-8

--- application/views/admin/questions_v.php
+++ application/views/admin/questions_v.php
@@ -25,11 +25,48 @@ require_once 'templates/header.php';
 						</div>
 					</div>
 				</div>
+                <div class="search_by_class">
+                    <h4>Search By</h4>
+                    <form action="<?php echo base_url('admin/questions/search') ?>" method="post">
+                        <div class="row">
+                            <div class="col-lg-2">
+                                <label>Country: </label>
+				                <?php echo form_dropdown('country_id',form_dropdown_cr(array('id','name'),'country'),'',array('class'=>'form-control')); ?>
+                            </div>
+                            <div class="col-lg-2">
+                                <label>Subject: </label>
+				                <?php echo form_dropdown('subject_id',form_dropdown_cr(array('id','name'),'subject'),'',array('class'=>'form-control')); ?>
+                            </div>
+                            <div class="col-lg-2">
+                                <label>Grade: </label>
+				                <?php echo form_dropdown('grade_id',form_dropdown_cr(array('id','name'),'grade'),'',array('class'=>'form-control')); ?>
+                            </div>
+                            <div class="col-lg-2">
+                                <label>Category: </label>
+				                <?php echo form_dropdown('category_id',form_dropdown_cr(array('id','name'),'category'),'',array('class'=>'form-control')); ?>
+                            </div>
+                            <div class="col-lg-2">
+                                <label>Topic: </label>
+				                <?php echo form_dropdown('topic_id',form_dropdown_cr(array('topic_id','topic_name'),'topics'),'',array('class'=>'form-control')); ?>
+                            </div>
+                            <div class="col-lg-2" style="margin-top: 30px;">
+                                <input type="submit" class="btn btn-small btn-primary" value="Search">
+                                <a href="<?php echo base_url('admin/questions'); ?>" class="btn btn-small btn-secondary">Clear</a>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+
 				<table class="table" data-paging="true" data-sorting="true" data-filtering="true" data-paging-size="20">
 					<thead>
 					<tr>
 						<th>Sl. No</th>
-						<th>Name</th>
+						<th>Question</th>
+						<th>Country</th>
+						<th>Subject</th>
+						<th>Grade</th>
+						<th>Category</th>
+						<th>Topic</th>
 						<th>Actions</th>
 					</tr>
 					</thead>
@@ -43,7 +80,12 @@ require_once 'templates/header.php';
 							<tr>
 								<td><?php echo $counter; ?></td>
 								<td><?php echo $question->question_name; ?></td>
-								<td><a href="<?php base_url('/admin/edit_question'); ?>/<?php $question->question_id; ?>">Edit</a></td>
+								<td><?php echo get_returnfield('country','id',$question->country_id,'name'); ?></td>
+								<td><?php echo get_returnfield('subject','id',$question->subject_id,'name'); ?></td>
+								<td><?php echo get_returnfield('grade','id',$question->grade_id,'name'); ?></td>
+								<td><?php echo get_returnfield('category','id',$question->category_id,'name'); ?></td>
+								<td><?php echo get_returnfield('topics','topic_id',$question->topic_id,'topic_name'); ?></td>
+								<td><a class="edit_link" href="<?php echo base_url('admin/edit_question/'.$question->question_id); ?>"><span data-feather="edit"></span> Edit</a> | <a class="delete_link" onclick="delete_data(<?php echo $question->question_id; ?>,'question_id','questions');" href="javascript:void(0)"><span data-feather="delete"></span> Delete</a></td>
 							</tr>
 							<?php
                             $counter++;
