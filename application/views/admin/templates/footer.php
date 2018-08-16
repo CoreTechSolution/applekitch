3b7e300d76253896aa0162c64576ab74594be21e
Rana <ranacoregen@gmail.com>
2018-08-16 20:12:29 +0530

    Phase-4 Point-8

--- application/views/admin/templates/footer.php
+++ application/views/admin/templates/footer.php
@@ -6,6 +6,7 @@
 <script src="<?php echo base_url('/assets/js/jquery.fancybox.pack.js'); ?>" type="text/javascript"></script>
 <script src="<?php echo base_url('/assets/js/sweetalert2.min.js'); ?>"></script>
 <script src="https://unpkg.com/feather-icons/dist/feather.min.js"></script>
+<!--<script src="https://cdn.jsdelivr.net/npm/gijgo@1.9.10/js/gijgo.min.js" type="text/javascript"></script>-->
 <script src="<?php echo base_url('assets/js/custom_admin.js'); ?>"></script>
 <script src="<?php echo base_url('assets/js/combinations.js'); ?>"></script>
 <script>
