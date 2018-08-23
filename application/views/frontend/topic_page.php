eec9cafbccbe7bc36a37fd53399febdcaf147bdc
Rana <ranacoregen@gmail.com>
2018-08-23 20:16:46 +0530

    Phase-6 Point-10

--- application/views/frontend/topic_page.php
+++ application/views/frontend/topic_page.php
@@ -3,112 +3,81 @@ $this->load->view('templates/header');
 ?>
     <div class="wrapper inner-pages">
         <div class="inner_page_banner">
-            <div class="container">
-                <div class="row">
-                    <h1>Learning <?php echo $banner_title; ?></h1>
-                </div>
-            </div>
 
-        </div>
-        <div class="top_tap_sub">
-            <div class="container">
-                <div class="row">
-                    <ul>
-						<?php //print_r($subjects); ?>
-						<?php foreach ($subjects as $subject){ ?>
-                            <li><span data-feather="book"></span> <a href=""><?php echo $subject->name; ?></a></li>
-						<?php } ?>
-                    </ul>
-                </div>
-            </div>
-        </div>
-        <div class="breadcrumbs_section">
-            <div class="container">
-                <div class="row">
-                    <ul class="breadcrumbs">
-                        <li class="breadcrumbs__item"><a href="<?= base_url(); ?>" class="breadcrumbs__element">Home</a></li>
-                        <li class="breadcrumbs__item"><a href="<?= base_url(); ?>" class="breadcrumbs__element">Grade</a></li>
-                        <li class="breadcrumbs__item breadcrumbs__item_active"><span class="breadcrumbs__element"><?php echo $banner_title; ?></span></li>
-                    </ul>
-                </div>
-            </div>
         </div>
         <div class="inner_page_content">
             <div class="container">
                 <div class="row">
-                    <div class="col-lg-12">
-                        <div class="score_time_div">
-                            <div class="score_ans">
-                                <div class="title">Question
-                                    Answered</div>
-                                <div class="content"><?= (!empty($this->session->userdata('score_ans')))? $this->session->userdata('score_ans'):'0' ?></div>
-                            </div>
-                            <div class="score_smart">
-                                <div class="title">Smart Score <br><sup>out of 100</sup> <a title="This is my span's tooltip message!" href="javascript:void(0)"><i class="fas fa-question-circle"></i></a></div>
-                                <div class="content"><?= (!empty($this->session->userdata('score_smart')))? $this->session->userdata('score_smart'):'0' ?></div>
-                            </div>
-                            <div class="score_time">
-                                <div class="title">Time Elapsed</div>
-                                <div class="content score_time_count">
-                                    <div class="row">
-                                        <div class="col-lg-4">
-                                            <span class="score_time_count_hr">00</span><br>
-                                            <span class="time_text">HR</span>
-                                        </div>
-                                        <div class="col-lg-4">
-                                            <span class="score_time_count_min">00</span><br>
-                                            <span class="time_text">MIN</span>
-                                        </div>
-                                        <div class="col-lg-4">
-                                            <span class="score_time_count_sec">00</span><br>
-                                            <span class="time_text">SEC</span>
-                                        </div>
-                                    </div>
-
-                                </div>
-                                <!--<sup><a href="" class="stop_question_timer" id="stop_question_timer">Stop</a></sup>-->
-                            </div>
-
-                        </div>
+                    <div class="subject_top_nav">
+                        <!--<ul>
+							<li><a href="#"><span data-feather="edit"></span> Maths</a></li>
+							<li><a href="#"><span data-feather="edit"></span> English</a></li>
+						</ul>-->
                     </div>
-                </div>
-                <div class="question_view">
-                    <div id="ans_label" class="cAns"><i class="fas fa-check"></i> Correct</div>
-                    <!--<div id="wrong_ans_label" class="wAns">Wrong Answer&#8230;</div>-->
-					<?php if(!empty($questions)){ ?>
-                        <form class="form qAns_form" method="post" action="" enctype="multipart/form-data">
-                            <input type="hidden" name="start" value="<?php echo $start; ?>" />
-                            <input type="hidden" name="grade_id" value="<?php echo $grade_id; ?>" />
-                            <input type="hidden" name="subject_id" value="<?php echo $subject_id; ?>" />
-                            <input type="hidden" name="topic_id" value="<?php echo $topic_id; ?>" />
+
+                    <h1><?php echo $title; ?></h1>
+					<?php if(!empty($topics)){
+						$cate_arrays=array(); $category='';
+						foreach ($topics as $topic){
+							$topic_key = $topic->topic_id;
+							$topic_value = $topic->category_id;
+							if(array_key_exists($topic_value, $cate_arrays)){
+								array_push($cate_arrays[$topic_value], $topic_key);
+							} else {
+								$cate_arrays[$topic_value] = array();
+								array_push($cate_arrays[$topic_value], $topic_key);
+							}
+						}
+						//print_r($cate_array);
+					} ?>
+					<?php //print_r($topics); ?>
+					<?php if(!empty($topics)){ ?>
+                        <div class="topic_list">
                             <div class="row">
-								<?php foreach ($questions as $question){ ?>
-                                    <input type="hidden" class="question_id" name="question_id" value="<?php echo $question->question_id; ?>">
-                                    <div class="col-lg-5">
-                                        <div class="question_count">Question <i class="fas fa-volume-up"></i></div>
-                                        <div class="question_display"><?php echo $question->question_name; ?></div>
+                                <div class="col-lg-2">
+                                    <div class="grade_side_list">
+                                        <ul>
+                                            <li><a class="grade_ci_1" href="">Reception</a></li>
+                                            <li><a class="grade_ci_2" href="#">Year 1</a></li>
+                                            <li><a class="grade_ci_3" href="#">Year 2</a></li>
+                                            <li><a class="grade_ci_4" href="#">Year 3</a></li>
+                                            <li><a class="grade_ci_5" href="#">Year 4</a></li>
+                                            <li><a class="grade_ci_6" href="#">Year 5</a></li>
+                                            <li><a class="grade_ci_7" href="#">Year 6</a></li>
+                                            <li><a class="grade_ci_8" href="#">Year 7</a></li>
+                                            <li><a class="grade_ci_9" href="#">Year 8</a></li>
+                                            <li><a class="grade_ci_10" href="#">Year 9</a></li>
+                                            <li><a class="grade_ci_11" href="#">Year 10</a></li>
+                                            <li><a class="grade_ci_12" href="#">Year 11</a></li>
+                                        </ul>
                                     </div>
-                                    <div class="col-lg-7">
-										<?php //echo $question->form_data; exit(); ?>
-										<?php $form_data=unserialize($question->form_data); ?>
-                                        <input type="hidden" name="question_option" value="<?= $form_data['question_option']; ?>">
-										<?php
-										$check_data['question_option']=$form_data['question_option'];
-										$check_data['form_data']=$form_data;
-										$this->load->view('frontend/template_part/frontend_answer_check',$check_data);
-										?>
-                                        <input type="submit" value="Submit" class="btn btn-small btn-outline-default qSubmit">
+                                </div>
+                                <div class="col-lg-10">
+                                    <div class="row">
+                                    <?php foreach ($cate_arrays as $cate_array=>$values){ ?>
+                                        <div class="col-lg-4">
+                                            <?php echo '<h4 class="listing_category">'.ucfirst(get_returnfield('category','id',$cate_array,'name')).'</h4>'; ?>
+                                            <ul>
+                                                <?php foreach ($values as $cate_arra){ ?>
+                                                    <li>
+                                                        <a href="<?php echo base_url('frontend/questions/'.$grade.'/'.$subject.'/'.$cate_arra) ?>">
+                                                            <span data-feather="edit"></span> <?php echo get_returnfield('topics','topic_id',$cate_arra,'topic_name'); ?>
+                                                        </a>
+                                                    </li>
+                                                <?php } ?>
+                                            </ul>
+                                        </div>
+                                    <?php } ?>
                                     </div>
-								<?php } ?>
+                                </div>
                             </div>
-                        </form>
+                        </div>
 					<?php } ?>
                 </div>
             </div>
         </div>
-        <div id="loading" class="loading">Loading&#8230;</div>
-    </div>
 
+    </div>
 
 <?php
 $this->load->view('templates/footer');
