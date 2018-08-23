b07283d6ef258ae13efc2baa148bf39cba9d0dbf
Rana <ranacoregen@gmail.com>
2018-08-23 20:14:59 +0530

    Phase-6 Point-10

--- application/views/home_page.php
+++ application/views/home_page.php
@@ -65,8 +65,8 @@ require_once 'templates/header.php';
                         <h3>Reception</h3>
                         <p class="circleWrap1-p">Here is a list of English and Math skills students learn in reception! These skills are organised into categories.</p>
                         <div class="circleWrap1-div">
-                            <a href="<?php echo base_url('frontend/grade/reception/math'); ?>"><p>Maths<br><small><?php echo get_question_count('','math','reception'); ?> Skills</small></p></a>
-                            <a href="<?php echo base_url('frontend/grade/reception/english'); ?>"><p>English<br><small><?php echo get_question_count('','english','reception'); ?> Skills</small></p></a>
+                            <a href="<?php echo base_url('frontend/topic/reception/math'); ?>"><p>Maths<br><small><?php echo get_question_count('','math','reception'); ?> Skills</small></p></a>
+                            <a href="<?php echo base_url('frontend/topic/reception/english'); ?>"><p>English<br><small><?php echo get_question_count('','english','reception'); ?> Skills</small></p></a>
                         </div>
                     </div>
                 </div>
@@ -75,8 +75,8 @@ require_once 'templates/header.php';
                         <h3>Year 1</h3>
                         <p class="circleWrap2-p">Adding and subtracting, telling time, measurement, categories, nouns, verb tense, time order and more.</p>
                         <div class="circleWrap2-div">
-                            <p>Maths<br><small>330 Skills</small></p>
-                            <p>English<br><small>112 Skills</small></p>
+                            <a href="<?php echo base_url('frontend/topic/year-1/math'); ?>"><p>Maths<br><small><?php echo get_question_count('','math','year-1'); ?> Skills</small></p></a>
+                            <a href="<?php echo base_url('frontend/topic/year-1/english'); ?>"><p>English<br><small><?php echo get_question_count('','english','year-1'); ?> Skills</small></p></a>
                         </div>
                     </div>
                 </div>
@@ -85,8 +85,8 @@ require_once 'templates/header.php';
                         <h3>Year 2</h3>
                         <p class="circleWrap3-p">Adding and subtracting, telling time, measurement, categories, nouns, verb tense, time order and more.</p>
                         <div class="circleWrap3-div">
-                            <p>Maths<br><small>330 Skills</small></p>
-                            <p>English<br><small>112 Skills</small></p>
+                            <a href="<?php echo base_url('frontend/topic/year-2/math'); ?>"><p>Maths<br><small><?php echo get_question_count('','math','year-2'); ?> Skills</small></p></a>
+                            <a href="<?php echo base_url('frontend/topic/year-2/english'); ?>"><p>English<br><small><?php echo get_question_count('','english','year-2'); ?> Skills</small></p></a>
                         </div>
                     </div>
                 </div>
@@ -95,8 +95,8 @@ require_once 'templates/header.php';
                         <h3>Year 3</h3>
                         <p class="circleWrap4-p">Adding and subtracting, telling time, measurement, categories, nouns, verb tense, time order and more.</p>
                         <div class="circleWrap4-div">
-                            <p>Maths<br><small>330 Skills</small></p>
-                            <p>English<br><small>112 Skills</small></p>
+                            <a href="<?php echo base_url('frontend/topic/year-3/math'); ?>"><p>Maths<br><small><?php echo get_question_count('','math','year-3'); ?> Skills</small></p></a>
+                            <a href="<?php echo base_url('frontend/topic/year-3/english'); ?>"><p>English<br><small><?php echo get_question_count('','english','year-3'); ?> Skills</small></p></a>
                         </div>
                     </div>
                 </div>
@@ -107,8 +107,8 @@ require_once 'templates/header.php';
                         <h3>Year 4</h3>
                         <p class="circleWrap5-p">Adding and subtracting, telling time, measurement, categories, nouns, verb tense, time order and more.</p>
                         <div class="circleWrap5-div">
-                            <p>Maths<br><small>330 Skills</small></p>
-                            <p>English<br><small>112 Skills</small></p>
+                            <a href="<?php echo base_url('frontend/topic/year-4/math'); ?>"><p>Maths<br><small><?php echo get_question_count('','math','year-4'); ?> Skills</small></p></a>
+                            <a href="<?php echo base_url('frontend/topic/year-4/english'); ?>"><p>English<br><small><?php echo get_question_count('','english','year-4'); ?> Skills</small></p></a>
                         </div>
                     </div>
                 </div>
@@ -117,8 +117,8 @@ require_once 'templates/header.php';
                         <h3>Year 5</h3>
                         <p class="circleWrap6-p">Adding and subtracting, telling time, measurement, categories, nouns, verb tense, time order and more.</p>
                         <div class="circleWrap6-div">
-                            <p>Maths<br><small>330 Skills</small></p>
-                            <p>English<br><small>112 Skills</small></p>
+                            <a href="<?php echo base_url('frontend/topic/year-5/math'); ?>"><p>Maths<br><small><?php echo get_question_count('','math','year-5'); ?> Skills</small></p></a>
+                            <a href="<?php echo base_url('frontend/topic/year-5/english'); ?>"><p>English<br><small><?php echo get_question_count('','english','year-5'); ?> Skills</small></p></a>
                         </div>
                     </div>
                 </div>
@@ -127,8 +127,8 @@ require_once 'templates/header.php';
                         <h3>Year 6</h3>
                         <p class="circleWrap7-p">Adding and subtracting, telling time, measurement, categories, nouns, verb tense, time order and more.</p>
                         <div class="circleWrap7-div">
-                            <p>Maths<br><small>330 Skills</small></p>
-                            <p>English<br><small>112 Skills</small></p>
+                            <a href="<?php echo base_url('frontend/topic/year-6/math'); ?>"><p>Maths<br><small><?php echo get_question_count('','math','year-6'); ?> Skills</small></p></a>
+                            <a href="<?php echo base_url('frontend/topic/year-6/english'); ?>"><p>English<br><small><?php echo get_question_count('','english','year-6'); ?> Skills</small></p></a>
                         </div>
                     </div>
                 </div>
@@ -137,8 +137,8 @@ require_once 'templates/header.php';
                         <h3>Year 7</h3>
                         <p class="circleWrap8-p">Adding and subtracting, telling time, measurement, categories, nouns, verb tense, time order and more.</p>
                         <div class="circleWrap8-div">
-                            <p>Maths<br><small>330 Skills</small></p>
-                            <p>English<br><small>112 Skills</small></p>
+                            <a href="<?php echo base_url('frontend/topic/year-7/math'); ?>"><p>Maths<br><small><?php echo get_question_count('','math','year-7'); ?> Skills</small></p></a>
+                            <a href="<?php echo base_url('frontend/topic/year-7/english'); ?>"><p>English<br><small><?php echo get_question_count('','english','year-7'); ?> Skills</small></p></a>
                         </div>
                     </div>
                 </div>
@@ -149,8 +149,8 @@ require_once 'templates/header.php';
                         <h3>Year 8</h3>
                         <p class="circleWrap9-p">Adding and subtracting, telling time, measurement, categories, nouns, verb tense, time order and more.</p>
                         <div class="circleWrap9-div">
-                            <p>Maths<br><small>330 Skills</small></p>
-                            <p>English<br><small>112 Skills</small></p>
+                            <a href="<?php echo base_url('frontend/topic/year-8/math'); ?>"><p>Maths<br><small><?php echo get_question_count('','math','year-8'); ?> Skills</small></p></a>
+                            <a href="<?php echo base_url('frontend/topic/year-8/english'); ?>"><p>English<br><small><?php echo get_question_count('','english','year-8'); ?> Skills</small></p></a>
                         </div>
                     </div>
                 </div>
@@ -159,8 +159,8 @@ require_once 'templates/header.php';
                         <h3>Year 9</h3>
                         <p class="circleWrap10-p">Adding and subtracting, telling time, measurement, categories, nouns, verb tense, time order and more.</p>
                         <div class="circleWrap10-div">
-                            <p>Maths<br><small>330 Skills</small></p>
-                            <p>English<br><small>112 Skills</small></p>
+                            <a href="<?php echo base_url('frontend/topic/year-9/math'); ?>"><p>Maths<br><small><?php echo get_question_count('','math','year-9'); ?> Skills</small></p></a>
+                            <a href="<?php echo base_url('frontend/topic/year-9/english'); ?>"><p>English<br><small><?php echo get_question_count('','english','year-9'); ?> Skills</small></p></a>
                         </div>
                     </div>
                 </div>
@@ -169,8 +169,8 @@ require_once 'templates/header.php';
                         <h3>Year 10</h3>
                         <p class="circleWrap11-p">Adding and subtracting, telling time, measurement, categories, nouns, verb tense, time order and more.</p>
                         <div class="circleWrap11-div">
-                            <p>Maths<br><small>330 Skills</small></p>
-                            <p>English<br><small>112 Skills</small></p>
+                            <a href="<?php echo base_url('frontend/topic/year-10/math'); ?>"><p>Maths<br><small><?php echo get_question_count('','math','year-10'); ?> Skills</small></p></a>
+                            <a href="<?php echo base_url('frontend/topic/year-10/english'); ?>"><p>English<br><small><?php echo get_question_count('','english','year-10'); ?> Skills</small></p></a>
                         </div>
                     </div>
                 </div>
@@ -179,8 +179,8 @@ require_once 'templates/header.php';
                         <h3>Year 11</h3>
                         <p class="circleWrap12-p">Adding and subtracting, telling time, measurement, categories, nouns, verb tense, time order and more.</p>
                         <div class="circleWrap12-div">
-                            <p>Maths<br><small>330 Skills</small></p>
-                            <p>English<br><small>112 Skills</small></p>
+                            <a href="<?php echo base_url('frontend/topic/year-11/math'); ?>"><p>Maths<br><small><?php echo get_question_count('','math','year-11'); ?> Skills</small></p></a>
+                            <a href="<?php echo base_url('frontend/topic/year-11/english'); ?>"><p>English<br><small><?php echo get_question_count('','english','year-11'); ?> Skills</small></p></a>
                         </div>
                     </div>
                 </div>
