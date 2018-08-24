76c91a6d6e837a1f1f1505e17de639ba5beffbb2
Rana <ranacoregen@gmail.com>
2018-08-24 15:01:53 +0530

    certificate

--- application/views/admin/templates/profile_sidebar.php
+++ application/views/admin/templates/profile_sidebar.php
@@ -1,61 +1,61 @@
 <div class="sidebar-sticky">
     <ul class="nav flex-column">
         <li class="nav-item">
-            <a class="nav-link<?php if($title == 'Dashboard') { ?> active<?php } ?>" href="<?= base_url('/admin'); ?>">
+            <a class="nav-link<?php if($title == 'Dashboard') { ?> active<?php } ?>" href="<?php echo base_url('/admin'); ?>">
                 <span data-feather="home"></span>
                 Dashboard <span class="sr-only">(current)</span>
             </a>
         </li>
         <li class="nav-item">
-            <a class="nav-link<?php if($title == 'Users') { ?> active<?php } ?>" href="<?= base_url('/admin/users'); ?>">
+            <a class="nav-link<?php if($title == 'Users') { ?> active<?php } ?>" href="<?php echo base_url('/admin/users'); ?>">
                 <span data-feather="users"></span>
                 Users
             </a>
         </li>
         <li class="nav-item">
-            <a class="nav-link<?php if($title == 'User Roles') { ?> active<?php } ?>" href="<?= base_url('/admin/user_roles'); ?>">
+            <a class="nav-link<?php if($title == 'User Roles') { ?> active<?php } ?>" href="<?php echo base_url('/admin/user_roles'); ?>">
                 <span data-feather="user"></span>
                 User Roles
             </a>
         </li>
         <li class="nav-item">
-            <a class="nav-link<?php if($title == 'Membership Plans') { ?> active<?php } ?>" href="<?= base_url('/admin/membership_plans'); ?>">
+            <a class="nav-link<?php if($title == 'Membership Plans') { ?> active<?php } ?>" href="<?php echo base_url('/admin/membership_plans'); ?>">
                 <span data-feather="info"></span>
                 Membership Plans
             </a>
         </li>
         <li class="nav-item">
-            <a class="nav-link<?php if($title == 'Grades' || $title == 'Add Grade') { ?> active<?php } ?>" href="<?= base_url('/admin/grade'); ?>">
+            <a class="nav-link<?php if($title == 'Grades' || $title == 'Add Grade') { ?> active<?php } ?>" href="<?php echo base_url('/admin/grade'); ?>">
                 <span data-feather="feather"></span>
                 Grade
             </a>
         </li>
         <li class="nav-item">
-            <a class="nav-link<?php if($title == 'Subjects' || $title == 'Add Subject') { ?> active<?php } ?>" href="<?= base_url('/admin/subjects'); ?>">
+            <a class="nav-link<?php if($title == 'Subjects' || $title == 'Add Subject') { ?> active<?php } ?>" href="<?php echo base_url('/admin/subjects'); ?>">
                 <span data-feather="book"></span>
                 Subject
             </a>
         </li>
         <li class="nav-item">
-            <a class="nav-link<?php if($title == 'Category' || $title == 'Add Category') { ?> active<?php } ?>" href="<?= base_url('/admin/category'); ?>">
+            <a class="nav-link<?php if($title == 'Category' || $title == 'Add Category') { ?> active<?php } ?>" href="<?php echo base_url('/admin/category'); ?>">
                 <span data-feather="list"></span>
                 Category
             </a>
         </li>
         <li class="nav-item">
-            <a class="nav-link<?php if($title == 'Topics' || $title == 'Add Topic') { ?> active<?php } ?>" href="<?= base_url('/admin/topics'); ?>">
+            <a class="nav-link<?php if($title == 'Topics' || $title == 'Add Topic') { ?> active<?php } ?>" href="<?php echo base_url('/admin/topics'); ?>">
                 <span data-feather="list"></span>
                 Topic
             </a>
         </li>
         <li class="nav-item">
-            <a class="nav-link<?php if($title == 'Certificates' || $title == 'Add Certificate') { ?> active<?php } ?>" href="<?= base_url('/admin/certifications'); ?>">
+            <a class="nav-link<?php if($title == 'Certificates' || $title == 'Add Certificate') { ?> active<?php } ?>" href="<?php echo base_url('/admin/certificates'); ?>">
                 <span data-feather="file"></span>
                 Certificates
             </a>
         </li>
         <li class="nav-item">
-            <a class="nav-link<?php if($title == 'Questions' || $title == 'Add Question' || $title == 'Add Question Type') { ?> active<?php } ?>" href="<?= base_url('/admin/questions'); ?>">
+            <a class="nav-link<?php if($title == 'Questions' || $title == 'Add Question' || $title == 'Add Question Type') { ?> active<?php } ?>" href="<?php echo base_url('/admin/questions'); ?>">
                 <span data-feather="help-circle"></span>
                 Questions
             </a>
