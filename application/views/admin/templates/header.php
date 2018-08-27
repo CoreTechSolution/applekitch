785658cba4d1c51397ece9d933e566923f9afb11
Jagannath <bhulbhal1981@gmail.com>
2018-08-27 15:32:35 +0530

    changes for add Page

--- application/views/admin/templates/header.php
+++ application/views/admin/templates/header.php
@@ -16,6 +16,8 @@
     <link href="<?php echo  base_url('/assets/css/jquery.fancybox.css'); ?>" rel="stylesheet">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
     <script src="<?php echo base_url('/assets/js/jquery.js'); ?>" type="text/javascript"></script>
+    <script src="<?php echo base_url('/assets/tinymce/tinymce.min.js'); ?>" ></script>
+    <!--<script src="https://cloud.tinymce.com/stable/tinymce.min.js"></script>-->
     <!--// GIJGO Grid System-->
 <!--    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" type="text/css" />
     <link href="https://cdn.jsdelivr.net/npm/gijgo@1.9.10/css/gijgo.min.css" rel="stylesheet" type="text/css" />
@@ -26,6 +28,20 @@
     <link rel="stylesheet" href="<?php echo base_url('/assets/css/jquery-ui.css'); ?>">
     <script>
         var base_url='<?php echo base_url(); ?>';
+
+        tinymce.init({
+            selector: ".textarea-no-styles",
+            height: 400,
+            theme: 'modern',
+            plugins: 'searchreplace autolink directionality visualblocks visualchars fullscreen image link media template codesample table charmap hr pagebreak nonbreaking anchor toc insertdatetime advlist lists textcolor wordcount imagetools  contextmenu colorpicker textpattern help code',
+            toolbar1: 'formatselect | bold italic strikethrough forecolor backcolor | link | alignleft aligncenter alignright alignjustify  | numlist bullist outdent indent  | removeformat | code',
+            image_advtab: true,
+            templates: [],
+            content_css: [
+                '//fonts.googleapis.com/css?family=Lato:300,300i,400,400i',
+                '//www.tinymce.com/css/codepen.min.css'
+            ]
+        });
     </script>
 </head>
 <body>
