01f78df50769419071bde9fff29d4613a11cf4c1
Jagannath <bhulbhal1981@gmail.com>
2018-08-27 12:34:37 +0530

    template added for "Add Page".

---
+++ application/views/admin/add_page.php
@@ -0,0 +1,34 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+require_once 'templates/header.php';
+?>
+	<div class="container-fluid">
+		<div class="row">
+			<nav class="col-md-2 d-none d-md-block bg-light sidebar">
+				<?php require_once 'templates/profile_sidebar.php'; ?>
+			</nav>
+
+			<main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4">
+				<div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+					<div class="container">
+						<div class="row">
+							<div class="col-lg-6">
+								<h1 class="h2"><?= $title; ?></h1>
+							</div>
+							<div class="col-lg-6">
+								<!--<div style="text-align: right;">
+									<a class="btn btn-primary btn-primary-green" href="<?/*= base_url('/admin/add_page'); */?>">Add Page</a>
+								</div>-->
+							</div>
+						</div>
+					</div>
+				</div>
+				<form class="form" method="post" action="">
+
+				</form>
+			</main>
+		</div>
+	</div>
+<?php
+require_once 'templates/footer.php';
+?>
