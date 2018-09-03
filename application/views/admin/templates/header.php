3c749fee5470eb1c7683795f69ca115eab07b069
Rana <ranacoregen@gmail.com>
2018-09-03 20:28:53 +0530

    modification

--- application/views/admin/templates/header.php
+++ application/views/admin/templates/header.php
@@ -9,7 +9,7 @@
 
     <title><?php echo $title; ?></title>
     <link rel="stylesheet" media="all" href="<?php echo  base_url('/assets/css/bootstrap.min.css'); ?>" />
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" rel="stylesheet">
+    <link href="<?php echo  base_url('/assets/css/font-awesome.css'); ?>" rel="stylesheet">
     <link href="<?php echo  base_url('/assets/css/dashboard.css'); ?>" rel="stylesheet">
     <link rel="stylesheet" media="all" href="<?php echo  base_url('/assets/css/footable.bootstrap.min.css'); ?>" />
     <link rel="stylesheet" href="<?php echo base_url('/assets/css/sweetalert2.min.css'); ?>">
