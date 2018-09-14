817a8d95b66057e7b55fd9b244c49d5523a45894
CoreTechSolution <tanujcoregen@gmail.com>
2018-09-14 15:37:36 +0530

    Tanuj's Commit

--- application/views/templates/profile_sidebar.php
+++ application/views/templates/profile_sidebar.php
@@ -1,6 +1,10 @@
 <div class="profile-sidebar">
 	<div class="profile-img">
+		<?php if(!empty($user_data['profile_img']) && $user_data['profile_img']!==''){ ?>
+            <img src="<?php echo $user_data['profile_img']; ?>" alt="" class="admin_edit_img_preview">
+		<?php } else { ?>
 		<img src="<?php echo base_url('/assets/images/noimg.png'); ?>">
+        <?php } ?>
 	</div>
 	<div class="profile-display-name"><?php echo $user_data['fname']; ?> <?php echo $user_data['lname'] ?></div>
 </div>
@@ -11,10 +15,10 @@
 		<li><a href="<?php echo base_url('/membership-plan'); ?>"><i class="fa fa-bookmark fa-fw" aria-hidden="true"></i> Membership Plans</a></li>
 		<li><a href="<?php echo base_url('/children'); ?>"><i class="fa fa-child" aria-hidden="true"></i> Children</a></li>
         <?php } ?>
-        <li><a href="<?php echo base_url('/certificates'); ?>"><i class="fa fa-user fa-fw" aria-hidden="true"></i> Certificates center</a></li>
-        <li><a href="<?php echo base_url('/awards'); ?>"><i class="fa fa-user fa-fw" aria-hidden="true"></i> Awards</a></li>
+        <li><a href="<?php echo base_url('/certificates'); ?>"><i class="fa fa-certificate fa-fw" aria-hidden="true"></i> Certificates center</a></li>
+        <li><a href="<?php echo base_url('/awards'); ?>"><i class="fa fa-trophy fa-fw" aria-hidden="true"></i> Awards</a></li>
 
-        <li class="dropdown"><a href="javacript:void(0)"><i class="fa fa-user fa-fw" aria-hidden="true"></i> Analytics &nbsp;<i class="fa fa-caret-down"></i></a>
+        <li class="dropdown"><a href="javacript:void(0);"><i class="fa fa-chart-pie fa-fw" aria-hidden="true"></i> Analytics &nbsp;<i class="fa fa-caret-down"></i></a>
             <ul class="dropdown_list">
                 <li><a href="<?php echo base_url('dashboard/usage'); ?>">Usage</a></li>
                 <li><a href="<?php echo base_url('dashboard/troublespot'); ?>">Trouble Spots</a></li>
