a37c6b2c956f74dfe08ca4595be1f9bd4e062eb3
CoreTechSolution <tanujcoregen@gmail.com>
2018-09-21 18:04:35 +0530

    Tanuj's Commit

--- application/views/templates/profile_sidebar.php
+++ application/views/templates/profile_sidebar.php
@@ -21,7 +21,11 @@
             <li><a href="<?php echo base_url('/certificates'); ?>"><i class="fa fa-certificate fa-fw" aria-hidden="true"></i> Certificates center</a></li>
         <?php } ?>
 
-        <li><a href="<?php echo base_url('/awards'); ?>"><i class="fa fa-trophy fa-fw" aria-hidden="true"></i> Awards</a></li>
+		<?php if(isUserType('Parent')==true || isUserType('Teacher')==true || isUserType('School')==true){ ?>
+            <li><a href="<?php echo base_url('/childaward'); ?>"><i class="fa fa-trophy fa-fw" aria-hidden="true"></i> Awards</a></li>
+		<?php } elseif (isUserType('Student')) { ?>
+            <li><a href="<?php echo base_url('/awards'); ?>"><i class="fa fa-trophy fa-fw" aria-hidden="true"></i> Awards</a></li>
+		<?php } ?>
 
         <li class="dropdown"><a href=""><i class="fa fa-chart-pie fa-fw" aria-hidden="true"></i> Analytics &nbsp;<i class="fa fa-caret-down"></i></a>
             <ul class="dropdown_list">
