9a1c79fe5f647c7494d59a5039220382401f4905
CoreTechSolution <tanujcoregen@gmail.com>
2018-09-28 18:31:11 +0530

    Tanuj's Commit

--- application/models/User_model.php
+++ application/models/User_model.php
@@ -228,6 +228,20 @@ class User_model extends CI_Model{
 			return false;
 		}
 	}
+	function get_certificates_default(){
+		//print_r($user_id); exit();
+		$this->db->select('*');
+		$this->db->where('default_status', 'true');
+		$this->db->from('student_ans_topic');
+		$query=$this->db->get();
+		//echo $this->db->last_query(); exit();
+		if($query->result()){
+			return $query->row();
+		}
+		else{
+			return false;
+		}
+	}
 	function get_certificates_by_id($id){
 		//print_r($user_id); exit();
 		$this->db->select('*');
