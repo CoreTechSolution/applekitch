edbb5066b1386e9d4f82cf228b39d8bc28e662fc
CoreTechSolution <CoreGenSaltlake1>
2018-09-28 15:27:44 +0530

    Rana's Commit

--- application/views/frontend/topic_page.php
+++ application/views/frontend/topic_page.php
@@ -70,9 +70,9 @@ $this->load->view('templates/header');
 							$grade_key = $topic->grade_id;
 							$topic_key = $topic->topic_id;
 							$cate_key = $topic->category_id;
-
+                            
 							if(array_key_exists($grade_key, $cate_arrays)){
-							    if(array_key_exists($cate_key,$cate_arrays)){
+							    if(array_key_exists($cate_key,$cate_arrays[$grade_key])){
 								    array_push($cate_arrays[$grade_key][$cate_key], $topic_key);
                                 } else{
 								    $cate_arrays[$grade_key][$cate_key] = array();
@@ -80,14 +80,16 @@ $this->load->view('templates/header');
                                 }
 
 							} else {
+								//$cate_arrays[$grade_key] = array();
 								$cate_arrays[$grade_key][$cate_key] = array();
 								array_push($cate_arrays[$grade_key][$cate_key], $topic_key);
 							}
-						}
-						//print_r($cate_array);
+                        }
+                        ksort($cate_arrays);
+						//print_r($cate_arrays); exit();
 					} ?>
 					<?php //print_r($topics); ?>
-					<?php if(!empty($topics)){ ?>
+					<?php if(!empty($cate_arrays)){ ?>
                         <div class="topic_list">
                             <div class="row">
                                 <div class="col-lg-2 col-md-2 col-sm-2 col-xs-2 bhoechie-tab-menu">
@@ -104,11 +106,12 @@ $this->load->view('templates/header');
                                     </div>
                                 </div>
                                 <div class="col-lg-10 col-md-10 col-sm-10 col-xs-10 bhoechie-tab">
-	                                <?php foreach ($cate_arrays as $cate_array1=>$values1){ ?>
+	                                <?php foreach ($cate_arrays as $cate_array1){ // get grade_ids ?>
+                                    <?php //print_r($cate_array1); ?>
                                         <div class="bhoechie-tab-content">
                                             <div class="row">
-				                                <?php foreach ($values1 as $cate_array=>$values){ ?>
-
+				                                <?php foreach ($cate_array1 as $cate_array=>$values){ // Get Categories ?>
+                                                    
                                                     <div class="col-lg-4">
 						                                <?php echo '<h4 class="listing_category">'.ucfirst(get_returnfield('category','id',$cate_array,'name')).'</h4>'; ?>
                                                         <ul>
