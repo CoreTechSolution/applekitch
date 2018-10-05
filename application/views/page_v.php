372266f7898622a529176b8e913830a070029c76
Jagannath <bhulbhal1981@gmail.com>
2018-10-05 18:28:11 +0530

    Change added for pages by Jagannath

---
+++ application/views/page_v.php
@@ -0,0 +1,18 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+require_once 'templates/header.php';
+?>
+<div class="pageBannerWrapper"></div>
+<div class="pageWrapper">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-12">
+				<?php echo '<h2>'.$page_item['page_title'].'</h2>'; ?>
+				<div class="pageContainer">
+					<?php echo $page_item['page_content']; ?>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<?php require_once 'templates/footer.php'; ?>
