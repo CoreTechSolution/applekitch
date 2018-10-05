c385c718826c0e1a11c8ce8a1ba6550342274a0d
Jagannath <bhulbhal1981@gmail.com>
2018-10-05 19:32:33 +0530

    Change added for pages by Jagannath

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -762,32 +762,6 @@ class Admin extends CI_Controller {
 		$this->load->view( 'admin/pages', $data );
 	}
 
-	public function slugify($text){
-		// replace non letter or digits by -
-		$text = preg_replace('~[^\pL\d]+~u', '-', $text);
-
-		// transliterate
-		$text = iconv('utf-8', 'us-ascii//TRANSLIT', $text);
-
-		// remove unwanted characters
-		$text = preg_replace('~[^-\w]+~', '', $text);
-
-		// trim
-		$text = trim($text, '-');
-
-		// remove duplicate -
-		$text = preg_replace('~-+~', '-', $text);
-
-		// lowercase
-		$text = strtolower($text);
-
-		if (empty($text)) {
-			return 'n-a';
-		}
-
-		return $text;
-	}
-
 	public function add_page(){
 		isLogin('admin');
 		/*$data = array(
