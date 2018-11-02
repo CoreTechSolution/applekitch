45b5509cecd90e9b4aa111ee8be77be43066dac8
CoreTechSolution <tanujcoregen@gmail.com>
2018-11-02 12:21:10 +0530

    Tanuj's Commit

--- application/views/page_v.php
+++ application/views/page_v.php
@@ -2,12 +2,19 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 require_once 'templates/header.php';
 ?>
-<div class="pageBannerWrapper"></div>
+<div class="pageBannerWrapper">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+				<?php echo '<h1 style="text-align: center; color: #FFFFFF;">'.$page_item['page'].'</h1>'; ?>
+            </div>
+        </div>
+    </div>
+</div>
 <div class="pageWrapper">
 	<div class="container">
 		<div class="row">
 			<div class="col-lg-12">
-				<?php echo '<h1>'.$page_item['page'].'</h1>'; ?>
 				<div class="pageContainer">
 					<?php echo $page_item['page_content']; ?>
 				</div>
