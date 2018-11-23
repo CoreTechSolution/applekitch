f61ee43d33f61884279428fddf5d2d99bcc70dfa
arghyamitra <CoreGenSaltlake1>
2018-11-23 13:21:20 +0530

    Arghya Commit

--- blog/wp-content/themes/standard-theme/index.php
+++
@@ -1,9 +0,0 @@
-<?php get_header(); ?>
-<div class="wrapper">
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-12"></div>
-        </div>
-    </div>
-</div>
-<?php get_footer(); ?>
--- blog/wp-content/themes/standard-theme/searchform.php
+++
@@ -1,4 +0,0 @@
-<form role="search" method="get" id="searchform" action="<?php echo home_url( '/' ); ?>">
-    <input type="text" id="searchbox" value="" name="s" placeholder="Search">
-    <input type="submit" id="searchBtn" value="&#xf002">
-</form>
